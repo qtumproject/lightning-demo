f9e7b21ffcbcb17cb5a5c66e3e479cd220b44c90
CodeFace <cnchenjun@qq.com>
2019-06-13 13:12:27 +0800

    update base58

--- eclair-core/src/main/scala/fr/acinq/eclair/bitcoin/Base58.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/bitcoin/Base58.scala
@@ -1,8 +1,8 @@
 package fr.acinq.bitcoin
 
-import java.math.BigInteger
+import java.nio.ByteOrder
 
-import scala.annotation.tailrec
+import scodec.bits.ByteVector
 
 /*
  * see https://en.bitcoin.it/wiki/Base58Check_encoding
@@ -29,47 +29,6 @@ object Base58 {
     val SecretKeySegnet = 158.toByte
   }
 
-  val alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-  // char -> value
-  val map = alphabet.zipWithIndex.toMap
-
-  /**
-    *
-    * @param input binary data
-    * @return the base-58 representation of input
-    */
-  def encode(input: Seq[Byte]): String = {
-    if (input.isEmpty) ""
-    else {
-      val big = new BigInteger(1, input.toArray)
-      val builder = new StringBuilder
-
-      @tailrec
-      def encode1(current: BigInteger): Unit = current match {
-        case BigInteger.ZERO => ()
-        case _ =>
-          val Array(x, remainder) = current.divideAndRemainder(BigInteger.valueOf(58L))
-          builder.append(alphabet.charAt(remainder.intValue))
-          encode1(x)
-      }
-
-      encode1(big)
-      input.takeWhile(_ == 0).map(_ => builder.append(alphabet.charAt(0)))
-      builder.toString().reverse
-    }
-  }
-
-  /**
-    *
-    * @param input base-58 encoded data
-    * @return the decoded data
-    */
-  def decode(input: String): BinaryData = {
-    val zeroes = input.takeWhile(_ == '1').map(_ => 0: Byte).toArray
-    val trim = input.dropWhile(_ == '1').toList
-    val decoded = trim.foldLeft(BigInteger.ZERO)((a, b) => a.multiply(BigInteger.valueOf(58L)).add(BigInteger.valueOf(map(b))))
-    if (trim.isEmpty) zeroes else zeroes ++ decoded.toByteArray.dropWhile(_ == 0) // BigInteger.toByteArray may add a leading 0x00
-  }
 }
 
 /**
@@ -90,7 +49,7 @@ object Base58 {
   *
   */
 object Base58Check {
-  def checksum(data: Seq[Byte]) = Crypto.hash256(data).take(4)
+  def checksum(data: ByteVector) = Crypto.hash256(data).take(4)
 
   /**
     * Encode data in Base58Check format.
@@ -100,9 +59,18 @@ object Base58Check {
     * @param data   date to be encoded
     * @return a Base58 string
     */
-  def encode(prefix: Byte, data: Seq[Byte]): String = {
-    val prefixAndData = prefix +: data
-    Base58.encode(prefixAndData ++ checksum(prefixAndData))
+  def encode(prefix: Byte, data: ByteVector): String = {
+    encode(ByteVector(prefix), data)
+  }
+
+  /**
+    *
+    * @param prefix version prefix (integer, as used with BIP32 ExtendedKeys for example)
+    * @param data   data to be encoded
+    * @return a Base58 String
+    */
+  def encode(prefix: Int, data: ByteVector): String = {
+    encode(Protocol.writeUInt32(prefix, ByteOrder.BIG_ENDIAN), data)
   }
 
   /**
@@ -111,9 +79,9 @@ object Base58Check {
     * @param data   data to be encoded
     * @return a Base58 String
     */
-  def encode(prefix: Seq[Byte], data: Seq[Byte]): String = {
+  def encode(prefix: ByteVector, data: ByteVector): String = {
     val prefixAndData = prefix ++ data
-    Base58.encode(prefixAndData ++ checksum(prefixAndData))
+    (prefixAndData ++ checksum(prefixAndData)).toBase58
   }
 
   /**
@@ -124,11 +92,37 @@ object Base58Check {
     * @param encoded encoded data
     * @return a (prefix, data) tuple
     */
-  def decode(encoded: String): (Byte, BinaryData) = {
-    val raw = Base58.decode(encoded)
+  def decode(encoded: String): (Byte, ByteVector) = {
+    val (prefix, data) = decodeWithPrefixLen(encoded, 1)
+    (prefix(0), data)
+  }
+
+  /**
+    * Decodes Base58 data that has been encoded with an integer prefix
+    *
+    * NB: requirement check will throw an IllegalArgumentException if the checksum that is part of the encoded data cannot be verified
+    *
+    * @param encoded encoded data
+    * @return a (prefix, data) tuple
+    */
+  def decodeWithIntPrefix(encoded: String): (Int, ByteVector) = {
+    val (prefix, data) = decodeWithPrefixLen(encoded, 4)
+    (Protocol.uint32(prefix.toArray, ByteOrder.BIG_ENDIAN).toInt, data)
+  }
+
+  /**
+    * Decodes Base58 data that has been encoded with several bytes prefix
+    *
+    * NB: requirement check will throw an IllegalArgumentException if the checksum that is part of the encoded data cannot be verified
+    *
+    * @param encoded encoded data
+    * @return a (prefix, data) tuple
+    */
+  def decodeWithPrefixLen(encoded: String, prefixLen: Int): (ByteVector, ByteVector) = {
+    val raw = ByteVector.fromValidBase58(encoded)
     val versionAndHash = raw.dropRight(4)
     val checksum = raw.takeRight(4)
     require(checksum == Base58Check.checksum(versionAndHash), s"invalid Base58Check data $encoded")
-    (versionAndHash(0), versionAndHash.tail)
+    (versionAndHash.take(prefixLen), versionAndHash.drop(prefixLen))
   }
 }
