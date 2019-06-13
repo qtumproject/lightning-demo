9519b3149872630243013ae66066cd160a261243
CodeFace <cnchenjun@qq.com>
2019-06-13 13:12:15 +0800

    update block header

--- eclair-core/src/main/scala/fr/acinq/eclair/bitcoin/BlockElectrum.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/bitcoin/BlockElectrum.scala
@@ -3,7 +3,7 @@ package fr.acinq.bitcoin
 import java.io.{InputStream, OutputStream}
 import java.math.BigInteger
 import java.nio.ByteOrder
-import java.util
+import scodec.bits._
 
 import fr.acinq.bitcoin.Protocol._
 
@@ -20,8 +20,8 @@ object BlockHeaderElectrum extends BtcSerializer[BlockHeaderElectrum] {
 
   override def write(input: BlockHeaderElectrum, out: OutputStream, protocolVersion: Long) = {
     writeUInt32(input.version.toInt, out)
-    writeBytes(input.hashPreviousBlock, out)
-    writeBytes(input.hashMerkleRoot, out)
+    writeBytes(input.hashPreviousBlock.toArray, out)
+    writeBytes(input.hashMerkleRoot.toArray, out)
     writeUInt32(input.time.toInt, out)
     writeUInt32(input.bits.toInt, out)
     writeUInt32(input.nonce.toInt, out)
@@ -49,10 +49,13 @@ object BlockHeaderElectrum extends BtcSerializer[BlockHeaderElectrum] {
   * @param bits              The calculated difficulty target being used for this block
   * @param nonce             The nonce used to generate this block… to allow variations of the header and compute different hashes
   */
-case class BlockHeaderElectrum(version: Long, hashPreviousBlock: BinaryData, hashMerkleRoot: BinaryData, time: Long, bits: Long, nonce: Long) extends BtcSerializable[BlockHeaderElectrum] {
+case class BlockHeaderElectrum(version: Long, hashPreviousBlock: ByteVector32, hashMerkleRoot: ByteVector32, time: Long, bits: Long, nonce: Long) extends BtcSerializable[BlockHeaderElectrum] {
   require(hashPreviousBlock.length == 32, "hashPreviousBlock must be 32 bytes")
   require(hashMerkleRoot.length == 32, "hashMerkleRoot must be 32 bytes")
-  lazy val hash: BinaryData = Crypto.hash256(BlockHeaderElectrum.write(this))
+  lazy val hash: ByteVector32 = Crypto.hash256(BlockHeaderElectrum.write(this))
+
+  // hash is reversed here (same as tx id)
+  lazy val blockId = hash.reverse
 
   override def serializer: BtcSerializer[BlockHeaderElectrum] = BlockHeaderElectrum
 }
@@ -60,7 +63,7 @@ case class BlockHeaderElectrum(version: Long, hashPreviousBlock: BinaryData, has
 object BlockElectrum extends BtcSerializer[BlockElectrum] {
   override def read(input: InputStream, protocolVersion: Long): BlockElectrum = {
     val raw = bytes(input, 80)
-    val header = BlockHeaderElectrum.read(raw)
+    val header = BlockHeaderElectrum.read(raw.toArray)
     BlockElectrum(header, readCollection[Transaction](input, protocolVersion))
   }
 
@@ -71,17 +74,17 @@ object BlockElectrum extends BtcSerializer[BlockElectrum] {
 
   override def validate(input: BlockElectrum): Unit = {
     BlockHeaderElectrum.validate(input.header)
-    require(util.Arrays.equals(input.header.hashMerkleRoot, MerkleTree.computeRoot(input.tx.map(_.hash))), "invalid block:  merkle root mismatch")
+    require(input.header.hashMerkleRoot ==  MerkleTree.computeRoot(input.tx.map(_.hash)), "invalid block:  merkle root mismatch")
     require(input.tx.map(_.txid).toSet.size == input.tx.size, "invalid block: duplicate transactions")
     input.tx.map(Transaction.validate)
   }
 
   // genesis blocks
   val LivenetGenesisBlock = {
-    val script = OP_PUSHDATA(writeUInt32(486604799L)) :: OP_PUSHDATA(BinaryData("04")) :: OP_PUSHDATA("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks".getBytes("UTF-8")) :: Nil
-    val scriptPubKey = OP_PUSHDATA("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") :: OP_CHECKSIG :: Nil
+    val script = OP_PUSHDATA(writeUInt32(486604799L)) :: OP_PUSHDATA(hex"04") :: OP_PUSHDATA(ByteVector("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks".getBytes("UTF-8"))) :: Nil
+    val scriptPubKey = OP_PUSHDATA(hex"04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") :: OP_CHECKSIG :: Nil
     BlockElectrum(
-      BlockHeaderElectrum(version = 1, hashPreviousBlock = Hash.Zeroes, hashMerkleRoot = "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a", time = 1231006505, bits = 0x1d00ffff, nonce = 2083236893),
+      BlockHeaderElectrum(version = 1, hashPreviousBlock = ByteVector32.Zeroes, hashMerkleRoot = ByteVector32(hex"3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a"), time = 1231006505, bits = 0x1d00ffff, nonce = 2083236893),
       List(
         Transaction(version = 1,
           txIn = List(TxIn.coinbase(script)),
@@ -113,7 +116,7 @@ object BlockElectrum extends BtcSerializer[BlockElectrum] {
     * @param tx coinbase transaction
     * @return the witness reserved value included in the input of this tx if any
     */
-  def witnessReservedValue(tx: Transaction) : Option[BinaryData] = tx.txIn(0).witness match {
+  def witnessReservedValue(tx: Transaction) : Option[ByteVector] = tx.txIn(0).witness match {
     case ScriptWitness(Seq(nonce)) if nonce.length == 32 => Some(nonce)
     case _ => None
   }
@@ -123,10 +126,10 @@ object BlockElectrum extends BtcSerializer[BlockElectrum] {
     * @param tx coinbase transaction
     * @return the witness commitment included in this transaction, if any
     */
-  def witnessCommitment(tx: Transaction): Option[BinaryData] = tx.txOut.map(o => Script.parse(o.publicKeyScript)).reverse.collectFirst {
+  def witnessCommitment(tx: Transaction): Option[ByteVector32] = tx.txOut.map(o => Script.parse(o.publicKeyScript)).reverse.collectFirst {
     // we've reversed the outputs because if there are more than one scriptPubKey matching the pattern, the one with
     // the highest output index is assumed to be the commitment.
-    case OP_RETURN :: OP_PUSHDATA(commitmentHeader, _) :: Nil if commitmentHeader.length == 36 && Protocol.uint32(commitmentHeader.take(4), ByteOrder.BIG_ENDIAN) == 0xaa21a9edL => commitmentHeader.takeRight(32)
+    case OP_RETURN :: OP_PUSHDATA(commitmentHeader, _) :: Nil if commitmentHeader.length == 36 && Protocol.uint32(commitmentHeader.take(4).toArray, ByteOrder.BIG_ENDIAN) == 0xaa21a9edL => ByteVector32(commitmentHeader.takeRight(32))
   }
 
   /**
@@ -139,7 +142,7 @@ object BlockElectrum extends BtcSerializer[BlockElectrum] {
     val coinbase = block.tx.head
     (witnessReservedValue(coinbase), witnessCommitment(coinbase)) match {
       case (Some(nonce), Some(commitment)) =>
-        val rootHash = MerkleTree.computeRoot(Hash.Zeroes +: block.tx.tail.map(_.whash))
+        val rootHash = MerkleTree.computeRoot(ByteVector32.Zeroes +: block.tx.tail.map(_.whash))
         val commitmentHash = Crypto.hash256(rootHash ++ nonce)
         commitment == commitmentHash
       case _ if block.tx.exists(_.hasWitness) => false // block has segwit transactions but no witness commitment
@@ -158,7 +161,7 @@ case class BlockElectrum(header: BlockHeaderElectrum, tx: Seq[Transaction]) exte
   lazy val hash = header.hash
 
   // hash is reversed here (same as tx id)
-  lazy val blockId = BinaryData(hash.reverse)
+  lazy val blockId = header.blockId
 
   override def serializer: BtcSerializer[BlockElectrum] = BlockElectrum
 }
